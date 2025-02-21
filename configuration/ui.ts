f86d46716af3e0ccacce0d467f449ef37b5defbf
Shelby Wray <wshelby@ad.unc.edu>
2025-02-20 21:11:34 -0500

    Update ui.ts
    messed up the footer (edited wrong line) so here's the right footer

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,9 +2,9 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
+export const FOOTER_MESSAGE: string = `Customized AI By Shelby @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with $AI, $Shelby's AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
