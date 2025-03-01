daaa3d702e147c771dfe857bccdbfdadf0b84dc9
Shelby Wray <wshelby@ad.unc.edu>
2025-03-01 10:35:59 -0500

    change chatbot name to marmalade

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `Shelby Wray`;
+export const OWNER_NAME: string = `Jammy Cat`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `Jammy-Cat`;
+export const AI_NAME: string = `Marmalade`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
